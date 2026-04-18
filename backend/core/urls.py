2ae016fe097fb19cd43a5aa7ab3c98f0085dacbb
Berry Muchocho <muchocho13@gmail.com>
2026-04-19 02:13:56 +0300

    add PUT endpoint to update task status with validation and task lifecycle support

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import process_request
-
+from . import views
 
 urlpatterns = [
     path('process_request/', process_request),
+    path("tasks/", views.get_tasks),
+    path("task/<str:task_code>/update/", views.update_task_status),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -46,3 +46,62 @@ def process_request(request):
 
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
+
+# Task dashboard API
+
+def get_tasks(request):
+    tasks = Task.objects.all().order_by("-created_at")
+
+    data = [
+        {
+            "task_code": t.task_code,
+            "intent": t.intent,
+            "risk_score": t.risk_score,
+            "status": t.status,
+            "assigned_team": t.assigned_team,
+            "created_at": t.created_at,
+        }
+
+        for t in tasks
+    ]
+
+    return JsonResponse(data, safe=False)
+
+# Update tast status endpoint (PUT)
+# Task lifecycle state includes: Pending - In Progress - Completed
+
+@csrf_exempt
+def update_task_status(request, task_code):
+    if request.method != "PUT":
+        return JsonResponse({"error": "Only PUT allowed"}, status=405)
+    
+    try: 
+        task = Task.objects.get(task_code=task_code)
+
+        data = json.loads(request.body)
+        new_status = data.get("status")
+
+        # To validate allowed statuses 
+
+        allowed_statuses = ["Pending", "In Progress", "Completed"]
+
+        if new_status not in allowed_statuses:
+            return JsonResponse({"error": f"Invalid status. Must be one of {allowed_statuses}"}, status=400)
+        
+        task.status = new_status
+        task.save()
+
+        return JsonResponse({
+            "task_code": task.task_code,
+            "status": task.status,
+            "message": "Task updated successfully"
+        })
+    
+    except Task.DoesNotExist:
+        return JsonResponse({"error": "Task not found"}, status=404)
+    
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+    
+    
+
